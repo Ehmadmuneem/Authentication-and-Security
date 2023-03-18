6bea3dd2a225b61a83ef14dd5f5c97c8c0b78633
Muneem Mushtaq <emuneem06@gmail.com>
2023-03-18 12:25:44 +0530

    second commit using md5

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const md5 = require("md5");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
@@ -26,11 +27,11 @@ const userSchema = new mongoose.Schema({
 //ENCRYPTION
 //Secret key
 //console.log(process.env.SECRET);
-//Passing encryption to user schema
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
+//Passing encryption to user schema, and fetching secret key from the .env file
+// userSchema.plugin(encrypt, {
+//   secret: process.env.SECRET,
+//   encryptedFields: ["password"],
+// });
 
 const User = mongoose.model("user", userSchema);
 
@@ -56,7 +57,7 @@ app.get("/submit", function (req, res) {
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({ email: username })
     .then(function (founduser) {
       if (founduser) {
@@ -77,7 +78,7 @@ app.post("/login", function (req, res) {
 app.post("/register", function (req, res) {
   let user = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
   user
     .save()
